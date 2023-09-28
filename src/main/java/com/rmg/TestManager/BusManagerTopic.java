452468c1282d19db4888336b25cba1a8399b729b
Prajwal Kumar J <126542734+PrajwakKumarJ@users.noreply.github.com>
2023-09-28 19:23:18 +0530

    Update BusManagerTopic.java

--- src/main/java/com/rmg/TestManager/BusManagerTopic.java
+++ src/main/java/com/rmg/TestManager/BusManagerTopic.java
@@ -22,10 +22,9 @@ import com.azure.messaging.servicebus.ServiceBusSenderClient;
 
 public class BusManagerTopic 
 {
-	static String connectionString = "Endpoint=sb://rmg-we-bigpapi-dev-sb-01.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=oeDD369qdXEBME7glf0aEAgPa8liE9uXXb7cJ3d5ra4=";
-	static String topicName = "rmg-we-bigpapi-dev-topic";
-	//static String queueName = "rmg-we-bigpapi-dev-queue";
-	static String subscriptionName = "rmg-we-bigpapi-dev-sub";
+	static String connectionString = "Endpoint=sb://rmg-we-bigpapi-dev-sb-01.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=r7arJdyC7bU4ygzMqAdbsddPH3sTnySyLMUZzX4kG/E=";
+	static String topicName = "rmg-we-bigpapi-dev-intersoft-topic-01";
+	static String subscriptionName = "rmg-we-bigpapi-dev-intersoft-sub-01";
 	
 	
 	/**
@@ -62,161 +61,7 @@ public class BusManagerTopic
 	}
 	
 	
-	
-	public List<ServiceBusMessage> createMessages()
-	{
-		System.out.println("3.2");
-		System.out.println("==========================================================================");
-	    System.out.println("Send Message List Block of Topic start");
-	    System.out.println("==========================================================================");
-	    // create a list of messages and return it to the caller
-	    ServiceBusMessage[] messages = {
-	    		new ServiceBusMessage("First message"),
-	    		new ServiceBusMessage("Second message"),
-	    		new ServiceBusMessage("Third message")
-	    };
-	    System.out.println("4.2");
-	    
-	    System.out.println("==========================================================================");
-	    System.out.println("Send Message List Block of Topic Ended");
-	    System.out.println("==========================================================================");
-	    
-	    return Arrays.asList(messages);
-	    
-	    
-	}
-	
-	/*
-	
-	public void sendMessageBatch()
-	{
-		System.out.println("5.2");
-		System.out.println("Send Message Batch Block of Topic start");
-		
-	    // create a Service Bus Sender client for the queue 
-	    ServiceBusSenderClient senderClient = new ServiceBusClientBuilder()
-	            .connectionString(connectionString)
-	            .sender()
-	            .topicName(topicName)
-	          //.queueName(queueName)
-	            .buildClient();
 
-	    // Creates an ServiceBusMessageBatch where the ServiceBus.
-	    ServiceBusMessageBatch messageBatch = senderClient.createMessageBatch();        
-
-		// create a list of messages
-	    List<ServiceBusMessage> listOfMessages = createMessages();
-
-	    // We try to add as many messages as a batch can fit based on the maximum size and send to Service Bus when
-	    // the batch can hold no more messages. Create a new batch for next set of messages and repeat until all
-	    // messages are sent.        
-	    for (ServiceBusMessage message : listOfMessages) {
-	        if (messageBatch.tryAddMessage(message)) {
-	            continue;
-	        }
-
-	        // The batch is full, so we create a new batch and send the batch.
-	        senderClient.sendMessages(messageBatch);
-	        System.out.println("Sent a batch of messages to the queue: " + topicName);
-
-	        // create a new batch
-	        messageBatch = senderClient.createMessageBatch();
-
-	        // Add that message that we couldn't before.
-	        if (!messageBatch.tryAddMessage(message)) {
-	            System.err.printf("Message is too large for an empty batch. Skipping. Max size: %s.", messageBatch.getMaxSizeInBytes());
-	        }
-	    }
-
-	    if (messageBatch.getCount() > 0) {
-	        senderClient.sendMessages(messageBatch);
-	        System.out.println("Sent a batch of messages to the queue: " + queueName);
-	    }
-	    System.out.println("6.2");
-	    System.out.println("Send Message Batch Block of Topic ended");
-	    
-	    //close the client
-	    senderClient.close();
-	}
-	
-	*/
-	
-	
-	
-	// handles received messages
-	public static void receiveMessages() throws InterruptedException
-	{
-		System.out.println("7.2");
-		System.out.println("==========================================================================");
-	    System.out.println("Receive Message Block of Topic started");
-	    System.out.println("==========================================================================");
-	    
-	    CountDownLatch countdownLatch = new CountDownLatch(1);
-
-	    // Create an instance of the processor through the ServiceBusClientBuilder
-	    ServiceBusProcessorClient processorClient = new ServiceBusClientBuilder()
-	        .connectionString(connectionString)
-	        .processor()
-	        .topicName(topicName)
-	      //.queueName(queueName)
-	        .subscriptionName(subscriptionName)
-	        .processMessage(t -> {
-				try {
-					processMessage(t);
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			})
-	        .processError(context -> processError(context, countdownLatch))
-	        .buildProcessorClient();
-	     
-	   
-	    System.out.println("Starting the processor");
-	    processorClient.start();
-
-	    TimeUnit.SECONDS.sleep(10);
-	    System.out.println("Stopping and closing the processor");
-	    processorClient.close(); 
-	    
-	    
-	    System.out.println("8.2");
-	    System.out.println("==========================================================================");
-	    System.out.println("Receive Message Block of Topic ended");
-	    System.out.println("==========================================================================");
-	    
-	}   
-	
-	
-	private static void processMessage(ServiceBusReceivedMessageContext context) throws IOException 
-	{
-		 System.out.println("9.2");
-		 System.out.println("process Message Block of Topic started");
-		 
-	    ServiceBusReceivedMessage message = context.getMessage();
-	    
-	    
-	    System.out.printf("Processing message. Session: %s, Sequence #: %s. Contents: %s%n", message.getMessageId(),
-	        message.getSequenceNumber(), message.getBody());
-	    
-	    
-//Writing message into file ReceiveOutput.json
-	    
-	    
-	    FileWriter myWriter = new FileWriter("output/ReceiveOutput.json");
-	      myWriter.write(context.getMessage().getBody().toString());
-	      myWriter.close();
-	    System.out.println("The Queue Message has been stored in file");  
-	    
-	    
-	    System.out.println("==========================================================================");
-	    System.out.println("process Message Block of Topic ended");
-	    System.out.println("==========================================================================");
-	    System.out.println("10.2");
-	}
-	
-	
-	
 	private static void processError(ServiceBusErrorContext context, CountDownLatch countdownLatch) 
 	{
 		System.out.println("11.2");
