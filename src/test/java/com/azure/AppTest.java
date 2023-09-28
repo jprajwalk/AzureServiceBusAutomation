cf2f09c4c34f7a8d7226079c15c78282fd582577
Prajwal Kumar J <126542734+PrajwakKumarJ@users.noreply.github.com>
2023-09-28 19:24:00 +0530

    Update AppTest.java

--- src/test/java/com/azure/AppTest.java
+++ src/test/java/com/azure/AppTest.java
@@ -17,32 +17,6 @@ import com.rmg.TestManager.DeleteDLQ;
 public class AppTest 
 {
 	
-	
-	/**
-	   * Rigorous Test :-)
-	   * @throws Throwable 
-	   */
-	@Test
-	  public void Queue() throws Throwable
-	  {
-		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-	    System.out.println("Starting Main Program  ||  App test ");
-	    System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-	    
-	    
-		  BusManagerQueue A = new BusManagerQueue();
-	  	  
-		  	//Sending a message to Queue
-		    	A.sendMessage("input/1P_PushAPI-Event-SampleRequest.json");
-		    	A.sendMessage("input/10P_PushAPI-Event_message with no mailpiece.json");
-		    	Thread.sleep(10000);
-		    	
-		    //receiving a message to Queue   	
-		      	
-		      	A.receiveMessages();    	
-	  }
-	
-
 	/**
      * Rigorous Test :-)
      * @throws Throwable 
@@ -57,7 +31,7 @@ public class AppTest
 	   //sending a message to topic with invalid data which will go to deadLetter Queue  	
 	   //B.sendMessage("input/1P_PushAPI-Event-SampleRequest.json");
 	      	try {
-	    		B.sendMessage("input/1P_PushAPI-Event-SampleRequest.json");
+	    		B.sendMessage("input/New.json");
 	    	} catch (Throwable e) {
 	    		// TODO Auto-generated catch block
 	    		e.printStackTrace();
@@ -69,44 +43,12 @@ public class AppTest
     	
        assertTrue( true );
     }
-  
-  /**
-   * Rigorous Test :-)
-   * @throws Throwable 
-   */
-@Test
-
-  public void DeadLetterQueue() throws Throwable
-  {
-	//System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-    //System.out.println("Starting Main Program  ||  App test ");
-    //System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-    
- 	
-	   //To send message to queue comment the below line A.receiveMessage(); command
-	    	
-	      	DeadLetterQueue D = new DeadLetterQueue();
-			D.ReceiveMessageFromDLQ();
-			Thread.sleep(10000);
-			System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-		    System.out.println("Ending Main Program  ||  App test ");
-		    System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-		    		
-			
-	    	 	
-	    	//C.deleteMessage();
-	    	
-    
-	    	
-  }
-  
-	
+ 
   
   public void main(String args[]) throws Throwable
   {
-	  Queue();
-	  Topic();
-	  //DeadLetterQueue();
+	 Topic();
+	
   }
  }
 
